83a784c57f6d3eb8dc0524af18ef1db474c9b7be
Paulo Teodoro <paulolimrs@outlook.com>
2023-02-20 15:46:27 -0300

    test: Project belongs a user unit test

---
+++ tests/Unit/ProjectTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ProjectTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_project_belongs_to_a_user() 
+    {
+        $project = Project::factory()->create();
+
+        $this->assertInstanceOf(User::class, $project->user);
+    }
+}
